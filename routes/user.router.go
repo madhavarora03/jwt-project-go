8ac59e659e27b5517ec30c0d33d5b159858a33c5
madhav <eragon.cube@outlook.com>
2025-04-10 19:57:06 +0530

    Work on routers

--- routes/user.router.go
+++ routes/user.router.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	controller "github.com/madhavarora03/golang-jwt-project/controllers"
+	"github.com/madhavarora03/golang-jwt-project/middleware"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+}
