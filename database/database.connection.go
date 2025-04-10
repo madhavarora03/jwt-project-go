bbc78055952d2eb9baaccb6be9021951e172750d
madhav <eragon.cube@outlook.com>
2025-04-10 20:53:20 +0530

    Daabase connection

--- database/database.connection.go
+++ database/database.connection.go
@@ -1,1 +1,41 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"log"
+	"os"
+)
+
+func DBInstance() *mongo.Client {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading the .env file")
+	}
+
+	MongoDBURI := os.Getenv("MONGODB_URI")
+
+	client, err := mongo.Connect(options.Client().ApplyURI(MongoDBURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	fmt.Println("Connected to MongoDB!")
+	return client
+}
+
+var Client *mongo.Client = DBInstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("cluster0").Collection(collectionName)
+	return collection
+}
